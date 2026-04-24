d7eb90ca63a40c0b9386f82f23776417fd46a9a3
pepitogumball-lang <pepitogumball@gmail.com>
2026-04-23 21:07:07 -0700

    Fix gdr.hpp include path

--- src/replay.hpp
+++ src/replay.hpp
@@ -2,22 +2,25 @@
 #define _replay_hpp
 
 #include <Geode/Geode.hpp>
-#include <gdr.hpp>
+#include <gdr/gdr.hpp>
 #include <fstream>
 #include <filesystem>
 
 using namespace geode::prelude;
 
-struct zInput : gdr::Input<> {
+#define ZBF_VERSION 3.0f
+
+struct zInput : gdr::Input {
     zInput() = default;
-    zInput(uint64_t frame, uint8_t button, bool player2, bool down)
+
+    zInput(int frame, int button, bool player2, bool down)
         : Input(frame, button, player2, down) {}
 };
 
-struct zReplay : gdr::Replay<zReplay, zInput> {
+struct zReplay : gdr::Replay {
     std::string name;
 
-    zReplay() : Replay("zBot", 1) {}
+    zReplay() : Replay("zBot", MOD_VERSION) {}
 
     void save() {
         author = GJAccountManager::get()->m_username;
@@ -27,39 +30,50 @@ struct zReplay : gdr::Replay<zReplay, zInput> {
         if (!std::filesystem::exists(dir)) {
             std::filesystem::create_directories(dir);
         }
-        
-        auto res = exportData();
-        if (res.isOk()) {
-            auto data = res.unwrap();
-            std::ofstream f(dir / (name + ".gdr"), std::ios::binary);
-            f.write(reinterpret_cast<const char*>(data.data()), data.size());
-            f.close();
-        }
+        std::ofstream f(dir / (name + ".gdr"), std::ios::binary);
+
+        auto data = exportData(false);
+
+        f.write(reinterpret_cast<const char*>(data.data()), data.size());
+        f.close();
     }
 
     static zReplay* fromFile(const std::string& fileName) {
         auto dir = geode::prelude::Mod::get()->getSaveDir() / "replays";
-        auto filePath = dir / (fileName.ends_with(".gdr") ? fileName : fileName + ".gdr");
-        
-        if (std::filesystem::exists(filePath)) {
-            auto res = zReplay::importData(filePath);
-            if (res.isOk()) {
-                zReplay* ret = new zReplay();
-                *static_cast<gdr::Replay<zReplay, zInput>*>(ret) = res.unwrap();
-                ret->name = fileName;
-                return ret;
+        if (std::filesystem::exists(dir) || std::filesystem::create_directories(dir)) {
+            std::ifstream f(dir / (fileName + ".gdr"), std::ios::binary);
+
+            if (!f.is_open()) {
+                f = std::ifstream(dir / fileName, std::ios::binary);
+                if (!f.is_open()) return nullptr;
             }
+
+            f.seekg(0, std::ios::end);
+            auto size = f.tellg();
+            f.seekg(0, std::ios::beg);
+
+            std::vector<uint8_t> data(size);
+            f.read(reinterpret_cast<char*>(data.data()), size);
+            f.close();
+
+            zReplay* ret = new zReplay();
+            *static_cast<gdr::Replay*>(ret) = gdr::Replay::importData(data);
+            ret->name = fileName;
+
+            return ret;
         }
+
         return nullptr;
     }
 
     void purgeAfter(int frame) {
-        inputs.erase(std::remove_if(inputs.begin(), inputs.end(), [frame](const zInput& input) {
-            return input.frame >= (uint64_t)frame;
+        inputs.erase(std::remove_if(inputs.begin(), inputs.end(), [frame](const gdr::Input& input) {
+            return input.frame >= frame;
         }), inputs.end());
     }
 
-    void addInput(uint64_t frame, uint8_t button, bool player2, bool down) {
+    void addInput(int frame, int button, bool player2, bool down) {
+        // log::info("Adding input: frame: {}, button: {}, player2: {}, down: {}", frame, button, player2, down);
         inputs.emplace_back(frame, button, player2, down);
     }
 };
